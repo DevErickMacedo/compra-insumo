d8bddd81e2129e3320fcf82d37e32f641c9a4132
Erick Macedo <erickmacedo@edu.unifor.br>
2026-05-25 14:47:59 -0300

    🎉 Commit Inicial

---
+++ src/main/java/com/feiralivre/compra_insumo/CompraInsumoApplication.java
@@ -0,0 +1,13 @@
+package com.feiralivre.compra_insumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraInsumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraInsumoApplication.class, args);
+	}
+
+}
